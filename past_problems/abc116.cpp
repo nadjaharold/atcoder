2926da1fecd8aef595b0b8d17ac401a552d6dc99
nadjaharold <rus0clov@gmail.com>
2020-06-30 22:43:38 +0900

    solved: abc116-c

--- past_problems/abc116.cpp
+++ past_problems/abc116.cpp
@@ -1,7 +1,7 @@
-// B - Collatz Problem
-// https://atcoder.jp/contests/abc116/tasks/abc116_b
-// Q. 
+// C - Grand Garden /
+// 数列内で右側んお花より伸びてる花を伸びてる高さ分だけ水やり。最後の花にも水をあげましょう。
 #include <bits/stdc++.h>
+using namespace std;
 #define lli long long int
 #define uli unsigned long long int
 #define rep(i, m, n) for (lli i = m; i < (n); i++)
@@ -9,19 +9,29 @@
 #define ALL(x) (x).begin(), (x).end()
 #define SIZE(x) ((lli)(x).size())
 #define MAX(x) *max_element(ALL(x))
-#define INF 1000000000000 //10^12
-#define MOD 10000007      //10^9+7
+#define MIN(x) *min_element(ALL(x))
+#define _GLIBCXX_DEBUG
+const lli INF = 2147483647;
+const lli MINF = -2147483648;
+const lli LINF = 9223372036854775807;
+const lli MOD = 1000000007; //10^9+7
+const double PI = acos(-1);
 #define SORT(n) sort(n.begin(), n.end())
 #define SORTR(n) sort(n.begin(), n.end(), greater<int>())
 #define REV(n) reverse(n.begin(), n.end())
-#define Vec(K, L, N, S) vector<L> K(N, S)
-#define rt sqrt
-using namespace std;
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#pragma GCC optimize("Ofast")
 using vi = vector<int>;
 using vvi = vector<vi>;
 using vvvi = vector<vvi>;
 using vlli = vector<lli>;
+using vvli = vector<vlli>;
 using vs = vector<string>;
+using vvs = vector<vs>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
 using ll = long long;
 // vector入力
 template <typename T>
@@ -43,46 +53,136 @@ ostream &operator<<(ostream &os, vector<T> &vec)
   // os << '}';
   return os;
 }
+// aよりもbが大きいならばaをbで更新する(更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する(更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
 
 int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  lli a, b, s, c, h, k, n, w, ans = 0, count = 0;
-  string t;
+  lli a, b, c, n, w, ans = 0, count = 0;
+  string s, t;
+  vector<pair<string, pair<lli, lli>>> pr;
   cin >> n;
-  vi d(1, n);
-  set<int> g;
-  g.insert(n);
-  while (count < 1000)
+  vlli h(n), e(110, 0);
+  rep(i, 0, n)
   {
-    if (n % 2 == 0)
-    {
-      n = n / 2;
-      d.push_back(n);
-      g.insert(n);
-    }
-    else
-    {
-      n = 3 * n + 1;
-      d.push_back(n);
-      g.insert(n);
-    }
-    count++;
+    cin >> h[i];
   }
-  rep(i, 0, d.size())
+  rep(i, 0, n - 1)
   {
-    rep(j, g.size(), d.size())
-    {
-      if (d[i] == d[j])
-      {
-        ans++;
-      }
-      if (d[i] == d[j] && ans >= 1)
-      {
-        cout << j + 1 << endl;
-        return 0;
-      }
-    }
+    if (h[i] > h[i + 1])
+      ans += h[i] - h[i + 1];
   }
+  ans += h[n - 1];
+  cout << ans << endl;
 }
+
+// B - Collatz Problem
+// https://atcoder.jp/contests/abc116/tasks/abc116_b
+
+// #include <bits/stdc++.h>
+// #define lli long long int
+// #define uli unsigned long long int
+// #define rep(i, m, n) for (lli i = m; i < (n); i++)
+// #define repe(i, m, n) for (lli i = m; i <= (n); i++)
+// #define ALL(x) (x).begin(), (x).end()
+// #define SIZE(x) ((lli)(x).size())
+// #define MAX(x) *max_element(ALL(x))
+// #define INF 1000000000000 //10^12
+// #define MOD 10000007      //10^9+7
+// #define SORT(n) sort(n.begin(), n.end())
+// #define SORTR(n) sort(n.begin(), n.end(), greater<int>())
+// #define REV(n) reverse(n.begin(), n.end())
+// #define Vec(K, L, N, S) vector<L> K(N, S)
+// #define rt sqrt
+// using namespace std;
+// using vi = vector<int>;
+// using vvi = vector<vi>;
+// using vvvi = vector<vvi>;
+// using vlli = vector<lli>;
+// using vs = vector<string>;
+// using ll = long long;
+// // vector入力
+// template <typename T>
+// istream &operator>>(istream &is, vector<T> &vec)
+// {
+//   for (T &x : vec)
+//     is >> x;
+//   return is;
+// }
+// // vector出力
+// template <typename T>
+// ostream &operator<<(ostream &os, vector<T> &vec)
+// {
+//   // os << '{';
+//   for (int i = 0; i < vec.size(); i++)
+//   {
+//     os << vec[i] << (i + 1 == vec.size() ? "" : "");
+//   }
+//   // os << '}';
+//   return os;
+// }
+
+// int main()
+// {
+//   ios::sync_with_stdio(false);
+//   cin.tie(0);
+//   lli a, b, s, c, h, k, n, w, ans = 0, count = 0;
+//   string t;
+//   cin >> n;
+//   vi d(1, n);
+//   set<int> g;
+//   g.insert(n);
+//   while (count < 1000)
+//   {
+//     if (n % 2 == 0)
+//     {
+//       n = n / 2;
+//       d.push_back(n);
+//       g.insert(n);
+//     }
+//     else
+//     {
+//       n = 3 * n + 1;
+//       d.push_back(n);
+//       g.insert(n);
+//     }
+//     count++;
+//   }
+//   rep(i, 0, d.size())
+//   {
+//     rep(j, g.size(), d.size())
+//     {
+//       if (d[i] == d[j])
+//       {
+//         ans++;
+//       }
+//       if (d[i] == d[j] && ans >= 1)
+//       {
+//         cout << j + 1 << endl;
+//         return 0;
+//       }
+//     }
+//   }
+// }
