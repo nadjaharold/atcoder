fd0f241d9ba3cabbe5b12ad031530a66cc2b0fe3
nadjaharold <rus0clov@gmail.com>
2020-07-05 12:41:11 +0900

    solved: abc094-c

--- past_problems/abc094.cpp
+++ past_problems/abc094.cpp
@@ -1,7 +1,7 @@
-// B - Toll Gates
-// https://atcoder.jp/contests/abc094/tasks/abc094_b
-// 大小比較と判定ごとの計算。ちょい複雑。
+// C - Many Medians
+// 中央値。中央値より大きいか否かで判別。
 #include <bits/stdc++.h>
+using namespace std;
 #define lli long long int
 #define uli unsigned long long int
 #define rep(i, m, n) for (lli i = m; i < (n); i++)
@@ -9,19 +9,29 @@
 #define ALL(x) (x).begin(), (x).end()
 #define SIZE(x) ((lli)(x).size())
 #define MAX(x) *max_element(ALL(x))
-#define INF 1000000000000 //10^12
-#define MOD 10000007      //10^9+7
+#define MIN(x) *min_element(ALL(x))
+#define _GLIBCXX_DEBUG
+const lli INF = 2147483647;
+const lli MINF = -2147483648;
+const lli LINF = 9223372036854775807;
+const lli MOD = 1000000007; //10^9+7
+const double PI = acos(-1);
 #define SORT(n) sort(n.begin(), n.end())
 #define SORTR(n) sort(n.begin(), n.end(), greater<int>())
 #define REV(n) reverse(n.begin(), n.end())
-#define Vec(K, L, N, S) vector<L> K(N, S)
-#define rt sqrt
-using namespace std;
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#pragma GCC optimize("Ofast")
 using vi = vector<int>;
 using vvi = vector<vi>;
 using vvvi = vector<vvi>;
 using vlli = vector<lli>;
+using vvli = vector<vlli>;
 using vs = vector<string>;
+using vvs = vector<vs>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
 using ll = long long;
 // vector入力
 template <typename T>
@@ -43,50 +53,148 @@ ostream &operator<<(ostream &os, vector<T> &vec)
   // os << '}';
   return os;
 }
+// aよりもbが大きいならばaをbで更新する(更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する(更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
 
 int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  lli b, c, h, k, n, m, x, w, ans = 0, count = 0, xd = 0, xa = 0, count2 = 0;
+  lli a, b, c, h, n, w = 0, ans = 0, count = 0;
   string s, t;
-  cin >> n >> m >> x;
-  vi a(m);
-  rep(i, 0, m)
+  vector<pair<string, pair<lli, lli>>> pr;
+  cin >> n;
+  vlli d(n), e;
+  rep(i, 0, n)
   {
-    cin >> a[i];
+    cin >> d[i];
+    e.pb(d[i]);
   }
-  rep(i, 0, m)
+  SORT(e);
+  a = (n - 1) / 2 + 1;
+
+  rep(i, 0, n)
   {
-    b = i;
-    if (a[i] < x)
+    if (d[i] > e[a - 1])
     {
-      xd++;
+      cout << e[a - 1] << endl;
     }
     else
     {
-      xa++;
+      cout << e[a] << endl;
     }
   }
-  // cout << xd << "  " << xa << endl;
-  rep(i, 0, m)
-  {
-    if (xd <= xa)
-    {
-      rep(j, 1, n)
-      {
-        if (a[i] == j && a[i] < x)
-          count++;
-      }
-    }
-    else
-    {
-      rep(j, 1, n)
-      {
-        if (a[i] == j && a[i] > x)
-          count2++;
-      }
-    }
-  }
-  cout << max(count, count2) << endl;
 }
+
+// B - Toll Gates
+// https://atcoder.jp/contests/abc094/tasks/abc094_b
+// 大小比較と判定ごとの計算。ちょい複雑。
+// #include <bits/stdc++.h>
+// #define lli long long int
+// #define uli unsigned long long int
+// #define rep(i, m, n) for (lli i = m; i < (n); i++)
+// #define repe(i, m, n) for (lli i = m; i <= (n); i++)
+// #define ALL(x) (x).begin(), (x).end()
+// #define SIZE(x) ((lli)(x).size())
+// #define MAX(x) *max_element(ALL(x))
+// #define INF 1000000000000 //10^12
+// #define MOD 10000007      //10^9+7
+// #define SORT(n) sort(n.begin(), n.end())
+// #define SORTR(n) sort(n.begin(), n.end(), greater<int>())
+// #define REV(n) reverse(n.begin(), n.end())
+// #define Vec(K, L, N, S) vector<L> K(N, S)
+// #define rt sqrt
+// using namespace std;
+// using vi = vector<int>;
+// using vvi = vector<vi>;
+// using vvvi = vector<vvi>;
+// using vlli = vector<lli>;
+// using vs = vector<string>;
+// using ll = long long;
+// // vector入力
+// template <typename T>
+// istream &operator>>(istream &is, vector<T> &vec)
+// {
+//   for (T &x : vec)
+//     is >> x;
+//   return is;
+// }
+// // vector出力
+// template <typename T>
+// ostream &operator<<(ostream &os, vector<T> &vec)
+// {
+//   // os << '{';
+//   for (int i = 0; i < vec.size(); i++)
+//   {
+//     os << vec[i] << (i + 1 == vec.size() ? "" : "");
+//   }
+//   // os << '}';
+//   return os;
+// }
+
+// int main()
+// {
+//   ios::sync_with_stdio(false);
+//   cin.tie(0);
+//   lli b, c, h, k, n, m, x, w, ans = 0, count = 0, xd = 0, xa = 0, count2 = 0;
+//   string s, t;
+//   cin >> n >> m >> x;
+//   vi a(m);
+//   rep(i, 0, m)
+//   {
+//     cin >> a[i];
+//   }
+//   rep(i, 0, m)
+//   {
+//     b = i;
+//     if (a[i] < x)
+//     {
+//       xd++;
+//     }
+//     else
+//     {
+//       xa++;
+//     }
+//   }
+//   // cout << xd << "  " << xa << endl;
+//   rep(i, 0, m)
+//   {
+//     if (xd <= xa)
+//     {
+//       rep(j, 1, n)
+//       {
+//         if (a[i] == j && a[i] < x)
+//           count++;
+//       }
+//     }
+//     else
+//     {
+//       rep(j, 1, n)
+//       {
+//         if (a[i] == j && a[i] > x)
+//           count2++;
+//       }
+//     }
+//   }
+//   cout << max(count, count2) << endl;
+// }
