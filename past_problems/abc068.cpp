b4d0b8b208cfcc3d63de16783cd083ff11aa3780
nadjaharold <rus0clov@gmail.com>
2020-07-05 20:39:34 +0900

    solved: abc068-c

--- past_problems/abc068.cpp
+++ past_problems/abc068.cpp
@@ -1,7 +1,7 @@
-// B - Break Number
-// https://atcoder.jp/contests/abc068/tasks/abc068_b
-// Q.苦戦。1<=n<=100の範囲で2で割り切れる回数が最多の数字を出力。
+// C - Cat Snuke and a Voyage
+// pairだとTLE, 島ごとの経路を二次元配列に持たせておく。ダイクストラ,最短経路系。
 #include <bits/stdc++.h>
+using namespace std;
 #define lli long long int
 #define uli unsigned long long int
 #define rep(i, m, n) for (lli i = m; i < (n); i++)
@@ -9,19 +9,29 @@
 #define ALL(x) (x).begin(), (x).end()
 #define SIZE(x) ((lli)(x).size())
 #define MAX(x) *max_element(ALL(x))
-#define INF 1000000000000 //10^12
-#define MOD 10000007      //10^9+7
+#define MIN(x) *min_element(ALL(x))
+#define _GLIBCXX_DEBUG
+const lli INF = 2147483647;
+const lli MINF = -2147483648;
+const lli LINF = 9223372036854775807;
+const lli MOD = 1000000007; //10^9+7
+const double PI = acos(-1);
 #define SORT(n) sort(n.begin(), n.end())
 #define SORTR(n) sort(n.begin(), n.end(), greater<int>())
 #define REV(n) reverse(n.begin(), n.end())
-#define Vec(K, L, N, S) vector<L> K(N, S)
-#define rt sqrt
-using namespace std;
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#pragma GCC optimize("Ofast")
 using vi = vector<int>;
 using vvi = vector<vi>;
 using vvvi = vector<vvi>;
 using vlli = vector<lli>;
+using vvli = vector<vlli>;
 using vs = vector<string>;
+using vvs = vector<vs>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
 using ll = long long;
 // vector入力
 template <typename T>
@@ -43,28 +53,132 @@ ostream &operator<<(ostream &os, vector<T> &vec)
   // os << '}';
   return os;
 }
+// aよりもbが大きいならばaをbで更新する(更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する(更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
 
 int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  lli b = 0, h, k, n, w, ans = 0, count = 0, tmp = 1;
+  lli a, b, c, h, n, m, w, ans = 0, count = 0;
   string s, t;
-  cin >> n;
-  repe(i, 1, n)
+  vector<pair<lli, lli>> pr;
+  vector<pair<lli, lli>> pr2;
+  cin >> n >> m;
+  vvli d(n);
+  rep(i, 0, m)
   {
-    b = i;
-    while (b % 2 == 0)
-    {
-      b /= 2;
-      count++;
-    }
-    if (count > ans)
-    {
-      ans = count;
-      tmp = i;
-    }
-    count = 0;
+    cin >> a >> b;
+    a--;
+    b--;
+    d[a].pb(b);
+    d[b].pb(a);
   }
-  cout << tmp << endl;
+
+  rep(j, 0, d[0].size())
+  {
+    c = d[0][j];
+    rep(k, 0, d[c].size())
+    {
+      if (d[c][k] == n - 1)
+      {
+        // cout << d[i][j] << endl;
+        cout << "POSSIBLE" << endl;
+        return 0;
+      }
+    }
+  }
+
+  cout << "IMPOSSIBLE" << endl;
 }
+
+// B - Break Number
+// https://atcoder.jp/contests/abc068/tasks/abc068_b
+// Q.苦戦。1<=n<=100の範囲で2で割り切れる回数が最多の数字を出力。
+// #include <bits/stdc++.h>
+// #define lli long long int
+// #define uli unsigned long long int
+// #define rep(i, m, n) for (lli i = m; i < (n); i++)
+// #define repe(i, m, n) for (lli i = m; i <= (n); i++)
+// #define ALL(x) (x).begin(), (x).end()
+// #define SIZE(x) ((lli)(x).size())
+// #define MAX(x) *max_element(ALL(x))
+// #define INF 1000000000000 //10^12
+// #define MOD 10000007      //10^9+7
+// #define SORT(n) sort(n.begin(), n.end())
+// #define SORTR(n) sort(n.begin(), n.end(), greater<int>())
+// #define REV(n) reverse(n.begin(), n.end())
+// #define Vec(K, L, N, S) vector<L> K(N, S)
+// #define rt sqrt
+// using namespace std;
+// using vi = vector<int>;
+// using vvi = vector<vi>;
+// using vvvi = vector<vvi>;
+// using vlli = vector<lli>;
+// using vs = vector<string>;
+// using ll = long long;
+// // vector入力
+// template <typename T>
+// istream &operator>>(istream &is, vector<T> &vec)
+// {
+//   for (T &x : vec)
+//     is >> x;
+//   return is;
+// }
+// // vector出力
+// template <typename T>
+// ostream &operator<<(ostream &os, vector<T> &vec)
+// {
+//   // os << '{';
+//   for (int i = 0; i < vec.size(); i++)
+//   {
+//     os << vec[i] << (i + 1 == vec.size() ? "" : "");
+//   }
+//   // os << '}';
+//   return os;
+// }
+
+// int main()
+// {
+//   ios::sync_with_stdio(false);
+//   cin.tie(0);
+//   lli b = 0, h, k, n, w, ans = 0, count = 0, tmp = 1;
+//   string s, t;
+//   cin >> n;
+//   repe(i, 1, n)
+//   {
+//     b = i;
+//     while (b % 2 == 0)
+//     {
+//       b /= 2;
+//       count++;
+//     }
+//     if (count > ans)
+//     {
+//       ans = count;
+//       tmp = i;
+//     }
+//     count = 0;
+//   }
+//   cout << tmp << endl;
+// }
