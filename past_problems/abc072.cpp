4810657a36089a7275964c1de9e7051d5906d120
nadjaharold <rus0clov@gmail.com>
2020-07-09 20:08:08 +0900

    solved: abc072.d

--- past_problems/abc072.cpp
+++ past_problems/abc072.cpp
@@ -1,7 +1,8 @@
-// C - Together
-// コーナーケース見逃さないように。
+// D - Derangement
+#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
+#define out(x) cout << x << '\n'
 #define lli long long int
 #define uli unsigned long long int
 #define rep(i, m, n) for (lli i = m; i < (n); i++)
@@ -22,7 +23,6 @@ const double PI = acos(-1);
 #define pb push_back
 #define eb emplace_back
 #define mp make_pair
-#pragma GCC optimize("Ofast")
 using vi = vector<int>;
 using vvi = vector<vi>;
 using vvvi = vector<vvi>;
@@ -33,6 +33,7 @@ using vvs = vector<vs>;
 using vb = vector<bool>;
 using vvb = vector<vb>;
 using ll = long long;
+//---------------------------------------------------------------------------------------------------
 // vector入力
 template <typename T>
 istream &operator>>(istream &is, vector<T> &vec)
@@ -75,36 +76,158 @@ bool chmin(T &a, const T &b)
   }
   return false;
 }
+//---------------------------------------------------------------------------------------------------
+lli a = 0, b = 0, c = 0, h = 0, n = 0, w = 0, ans = 0, count = 0;
+string s = "", t = "";
+vector<pair<lli, lli>> pr;
+map<lli, lli> mp;
+set<lli> st;
 
-int main()
+//---------------------------------------------------------------------------------------------------
+lli solve()
 {
+  lli a = 0, b = 0, c = 0, h = 0, n = 0, w = 0, ans = 0, count = 0;
+  string s = "", t = "";
+  vector<pair<lli, lli>> pr;
+  map<lli, lli> mp;
+  set<lli> st;
+
   ios::sync_with_stdio(false);
-  cin.tie(0);
-  lli a, b, c, h, n, w, ans = 0, count = 0;
-  string s, t;
-  vector<pair<string, pair<lli, lli>>> pr;
+  cin.tie(nullptr);
   cin >> n;
-  vlli d(n), e(100010, 0), f(100010, 0), g(100010, 0), k, l(100010, 0);
+  vlli d(n);
   rep(i, 0, n)
   {
     cin >> d[i];
-    // e[d[i] - 1]++;
-    // f[d[i] + 1]++;
-    // g[d[i]]++;
-    if (d[i] >= 1)
-    {
-      l[d[i] - 1]++;
-    }
-    l[d[i] + 1]++;
-    l[d[i]]++;
   }
-  // SORTR(e);
-  // SORTR(f);
-  // SORTR(g);
-  SORTR(l);
-  // k.pb(e[0]);
-  // k.pb(f[0]);
-  // k.pb(g[0]);
-  // SORTR(k);
-  cout << l[0] << endl;
+  rep(i, 0, n)
+  {
+    if (d[i] == i + 1)
+    {
+      count++;
+      if (d[i + 1] == i + 2 && i != n - 1)
+      {
+        ans++;
+      }
+    }
+  }
+  out(ans);
+  return count;
 }
+
+//---------------------------------------------------------------------------------------------------
+signed main()
+{
+  out(solve());
+}
+
+// C - Together
+// コーナーケース見逃さないように。
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define lli long long int
+// #define uli unsigned long long int
+// #define rep(i, m, n) for (lli i = m; i < (n); i++)
+// #define repe(i, m, n) for (lli i = m; i <= (n); i++)
+// #define ALL(x) (x).begin(), (x).end()
+// #define SIZE(x) ((lli)(x).size())
+// #define MAX(x) *max_element(ALL(x))
+// #define MIN(x) *min_element(ALL(x))
+// #define _GLIBCXX_DEBUG
+// const lli INF = 2147483647;
+// const lli MINF = -2147483648;
+// const lli LINF = 9223372036854775807;
+// const lli MOD = 1000000007; //10^9+7
+// const double PI = acos(-1);
+// #define SORT(n) sort(n.begin(), n.end())
+// #define SORTR(n) sort(n.begin(), n.end(), greater<int>())
+// #define REV(n) reverse(n.begin(), n.end())
+// #define pb push_back
+// #define eb emplace_back
+// #define mp make_pair
+// #pragma GCC optimize("Ofast")
+// using vi = vector<int>;
+// using vvi = vector<vi>;
+// using vvvi = vector<vvi>;
+// using vlli = vector<lli>;
+// using vvli = vector<vlli>;
+// using vs = vector<string>;
+// using vvs = vector<vs>;
+// using vb = vector<bool>;
+// using vvb = vector<vb>;
+// using ll = long long;
+// // vector入力
+// template <typename T>
+// istream &operator>>(istream &is, vector<T> &vec)
+// {
+//   for (T &x : vec)
+//     is >> x;
+//   return is;
+// }
+// // vector出力
+// template <typename T>
+// ostream &operator<<(ostream &os, vector<T> &vec)
+// {
+//   // os << '{';
+//   for (int i = 0; i < vec.size(); i++)
+//   {
+//     os << vec[i] << (i + 1 == vec.size() ? "" : "");
+//   }
+//   // os << '}';
+//   return os;
+// }
+// // aよりもbが大きいならばaをbで更新する(更新されたならばtrueを返す)
+// template <typename T>
+// bool chmax(T &a, const T &b)
+// {
+//   if (a < b)
+//   {
+//     a = b; // aをbで更新
+//     return true;
+//   }
+//   return false;
+// }
+// // aよりもbが小さいならばaをbで更新する(更新されたならばtrueを返す)
+// template <typename T>
+// bool chmin(T &a, const T &b)
+// {
+//   if (a > b)
+//   {
+//     a = b; // aをbで更新
+//     return true;
+//   }
+//   return false;
+// }
+
+// int main()
+// {
+//   ios::sync_with_stdio(false);
+//   cin.tie(0);
+//   lli a, b, c, h, n, w, ans = 0, count = 0;
+//   string s, t;
+//   vector<pair<string, pair<lli, lli>>> pr;
+//   cin >> n;
+//   vlli d(n), e(100010, 0), f(100010, 0), g(100010, 0), k, l(100010, 0);
+//   rep(i, 0, n)
+//   {
+//     cin >> d[i];
+//     // e[d[i] - 1]++;
+//     // f[d[i] + 1]++;
+//     // g[d[i]]++;
+//     if (d[i] >= 1)
+//     {
+//       l[d[i] - 1]++;
+//     }
+//     l[d[i] + 1]++;
+//     l[d[i]]++;
+//   }
+//   // SORTR(e);
+//   // SORTR(f);
+//   // SORTR(g);
+//   SORTR(l);
+//   // k.pb(e[0]);
+//   // k.pb(f[0]);
+//   // k.pb(g[0]);
+//   // SORTR(k);
+//   cout << l[0] << endl;
+// }
