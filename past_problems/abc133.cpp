8874ee6c0e839089e4309ebf8ffb5757debf775f
nadjaharold <rus0clov@gmail.com>
2020-07-21 22:55:59 +0900

    solved: abc133-a

--- past_problems/abc133.cpp
+++ past_problems/abc133.cpp
@@ -1,104 +1,104 @@
 // C - Remainder Minimization 2019
 // 0になるときあるよねって話。l, r区間内のi*jを2019で割ったときの最小値。2017like numberに似てる。
-#include <bits/stdc++.h>
-using namespace std;
-#define lli long long int
-#define uli unsigned long long int
-#define rep(i, m, n) for (lli i = m; i < (n); i++)
-#define repe(i, m, n) for (lli i = m; i <= (n); i++)
-#define ALL(x) (x).begin(), (x).end()
-#define SIZE(x) ((lli)(x).size())
-#define MAX(x) *max_element(ALL(x))
-#define MIN(x) *min_element(ALL(x))
-#define _GLIBCXX_DEBUG
-const lli INF = 2147483647;
-const lli MINF = -2147483648;
-const lli LINF = 9223372036854775807;
-const lli MOD = 1000000007; //10^9+7
-const double PI = acos(-1);
-#define SORT(n) sort(n.begin(), n.end())
-#define SORTR(n) sort(n.begin(), n.end(), greater<int>())
-#define REV(n) reverse(n.begin(), n.end())
-#define pb push_back
-#define eb emplace_back
-#define mp make_pair
-#pragma GCC optimize("Ofast")
-using vi = vector<int>;
-using vvi = vector<vi>;
-using vvvi = vector<vvi>;
-using vlli = vector<lli>;
-using vvli = vector<vlli>;
-using vs = vector<string>;
-using vvs = vector<vs>;
-using vb = vector<bool>;
-using vvb = vector<vb>;
-using ll = long long;
-// vector入力
-template <typename T>
-istream &operator>>(istream &is, vector<T> &vec)
-{
-  for (T &x : vec)
-    is >> x;
-  return is;
-}
-// vector出力
-template <typename T>
-ostream &operator<<(ostream &os, vector<T> &vec)
-{
-  // os << '{';
-  for (int i = 0; i < vec.size(); i++)
-  {
-    os << vec[i] << (i + 1 == vec.size() ? "" : "");
-  }
-  // os << '}';
-  return os;
-}
-// aよりもbが大きいならばaをbで更新する(更新されたならばtrueを返す)
-template <typename T>
-bool chmax(T &a, const T &b)
-{
-  if (a < b)
-  {
-    a = b; // aをbで更新
-    return true;
-  }
-  return false;
-}
-// aよりもbが小さいならばaをbで更新する(更新されたならばtrueを返す)
-template <typename T>
-bool chmin(T &a, const T &b)
-{
-  if (a > b)
-  {
-    a = b; // aをbで更新
-    return true;
-  }
-  return false;
-}
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define lli long long int
+// #define uli unsigned long long int
+// #define rep(i, m, n) for (lli i = m; i < (n); i++)
+// #define repe(i, m, n) for (lli i = m; i <= (n); i++)
+// #define ALL(x) (x).begin(), (x).end()
+// #define SIZE(x) ((lli)(x).size())
+// #define MAX(x) *max_element(ALL(x))
+// #define MIN(x) *min_element(ALL(x))
+// #define _GLIBCXX_DEBUG
+// const lli INF = 2147483647;
+// const lli MINF = -2147483648;
+// const lli LINF = 9223372036854775807;
+// const lli MOD = 1000000007; //10^9+7
+// const double PI = acos(-1);
+// #define SORT(n) sort(n.begin(), n.end())
+// #define SORTR(n) sort(n.begin(), n.end(), greater<int>())
+// #define REV(n) reverse(n.begin(), n.end())
+// #define pb push_back
+// #define eb emplace_back
+// #define mp make_pair
+// #pragma GCC optimize("Ofast")
+// using vi = vector<int>;
+// using vvi = vector<vi>;
+// using vvvi = vector<vvi>;
+// using vlli = vector<lli>;
+// using vvli = vector<vlli>;
+// using vs = vector<string>;
+// using vvs = vector<vs>;
+// using vb = vector<bool>;
+// using vvb = vector<vb>;
+// using ll = long long;
+// // vector入力
+// template <typename T>
+// istream &operator>>(istream &is, vector<T> &vec)
+// {
+//   for (T &x : vec)
+//     is >> x;
+//   return is;
+// }
+// // vector出力
+// template <typename T>
+// ostream &operator<<(ostream &os, vector<T> &vec)
+// {
+//   // os << '{';
+//   for (int i = 0; i < vec.size(); i++)
+//   {
+//     os << vec[i] << (i + 1 == vec.size() ? "" : "");
+//   }
+//   // os << '}';
+//   return os;
+// }
+// // aよりもbが大きいならばaをbで更新する(更新されたならばtrueを返す)
+// template <typename T>
+// bool chmax(T &a, const T &b)
+// {
+//   if (a < b)
+//   {
+//     a = b; // aをbで更新
+//     return true;
+//   }
+//   return false;
+// }
+// // aよりもbが小さいならばaをbで更新する(更新されたならばtrueを返す)
+// template <typename T>
+// bool chmin(T &a, const T &b)
+// {
+//   if (a > b)
+//   {
+//     a = b; // aをbで更新
+//     return true;
+//   }
+//   return false;
+// }
 
-int main()
-{
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  lli a, b, c, h, n, w, l, r, ans = 0, count = 2020;
-  string s, t;
-  vector<pair<string, pair<lli, lli>>> pr;
-  cin >> l >> r;
-  repe(i, l, r)
-  {
-    repe(j, i + 1, r)
-    {
-      ans = (i * j) % 2019;
-      if (ans == 0)
-      {
-        cout << 0 << endl;
-        return 0;
-      }
-      count = min(count, ans);
-    }
-  }
-  cout << count << endl;
-}
+// int main()
+// {
+//   ios::sync_with_stdio(false);
+//   cin.tie(0);
+//   lli a, b, c, h, n, w, l, r, ans = 0, count = 2020;
+//   string s, t;
+//   vector<pair<string, pair<lli, lli>>> pr;
+//   cin >> l >> r;
+//   repe(i, l, r)
+//   {
+//     repe(j, i + 1, r)
+//     {
+//       ans = (i * j) % 2019;
+//       if (ans == 0)
+//       {
+//         cout << 0 << endl;
+//         return 0;
+//       }
+//       count = min(count, ans);
+//     }
+//   }
+//   cout << count << endl;
+// }
 
 // https://atcoder.jp/contests/abc133/tasks/abc133_b
 // むずすぎ。平方根、ルート、sqrt。多重ループ。repマクロ。sqrtの切り捨てに注意。
@@ -172,3 +172,105 @@ int main()
 //   }
 //   cout << ans << endl;
 // }
+
+// A - T or T
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+#define out(x) cout << x << '\n'
+#define lli long long int
+#define uli unsigned long long int
+#define rep(i, m, n) for (lli i = m; i < (n); i++)
+#define repe(i, m, n) for (lli i = m; i <= (n); i++)
+#define ALL(x) (x).begin(), (x).end()
+#define SIZE(x) ((lli)(x).size())
+#define MAX(x) *max_element(ALL(x))
+#define MIN(x) *min_element(ALL(x))
+#define _GLIBCXX_DEBUG
+const lli INF = 2147483647;
+const lli MINF = -2147483648;
+const lli LINF = 9223372036854775807;
+const lli MOD = 1000000007; //10^9+7
+const double PI = acos(-1);
+#define SORT(n) sort(n.begin(), n.end())
+#define SORTR(n) sort(n.begin(), n.end(), greater<int>())
+#define REV(n) reverse(n.begin(), n.end())
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vlli = vector<lli>;
+using vvli = vector<vlli>;
+using vs = vector<string>;
+using vvs = vector<vs>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using ll = long long;
+//---------------------------------------------------------------------------------------------------
+// vector入力
+template <typename T>
+istream &operator>>(istream &is, vector<T> &vec)
+{
+  for (T &x : vec)
+    is >> x;
+  return is;
+}
+// vector出力
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &vec)
+{
+  // os << '{';
+  for (int i = 0; i < vec.size(); i++)
+  {
+    os << vec[i] << (i + 1 == vec.size() ? "" : "");
+  }
+  // os << '}';
+  return os;
+}
+// aよりもbが大きいならばaをbで更新する(更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する(更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+//---------------------------------------------------------------------------------------------------
+lli a = 0, b = 0, c = 0, h = 0, n = 0, w = 0, ans = 0, count = 0;
+string s = "", t = "";
+vector<pair<lli, lli>> pr;
+map<lli, lli> mp;
+set<lli> st;
+
+//---------------------------------------------------------------------------------------------------
+lli solve()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cin >> n >> a >> b;
+  ans = min(n * a, b);
+  return ans;
+}
+
+//---------------------------------------------------------------------------------------------------
+signed main()
+{
+  out(solve());
+}
