23bb75d1d5b11178bc919d94b097c7b5bf7c27e0
nadjaharold <rus0clov@gmail.com>
2020-07-26 12:56:04 +0900

    solved: m-solutions2020-d

--- contests/m-solutions2020/d.cpp
+++ contests/m-solutions2020/d.cpp
@@ -92,61 +92,71 @@ void solve()
   rep(i, 0, n)
   {
     cin >> d[i];
-    e[i] = d[i];
+    // e[i] = d[i];
   }
-  SORT(e);
-  a = e[n / 2];
+  // SORT(e);
+  // a = e[n / 2];
   ans = 1000;
+
+  rep(i, 0, n - 1)
+  {
+    if (d[i] < d[i + 1])
+    {
+      a = ans / d[i];
+      ans += a * (d[i + 1] - d[i]);
+    }
+  }
+
   // cout << e << endl;
   // cout << a << endl;
-  bool z = false, xx = false, yy = false;
-  rep(i, 0, n)
-  {
-    b = d[i];
-    rep(j, i + 1, n)
-    {
-      if (d[j] > b)
-      {
-        z = true;
-        // cout << "e: " << d[j] << endl;
-      }
-    }
-    // cout << "z: " << z << endl;
-    if (d[i] < a && z == true)
-    {
-      count += (ans / d[i]);
-      ans -= (ans / d[i]) * d[i];
-    }
-    else if (d[i] >= a)
-    {
-      // cout << "ans2: " << ans << " count: " << count << endl;
-      rep(j, i + 1, n)
-      {
-        if (d[j] > b)
-        {
-          xx = true;
-          // cout << "e: " << d[j] << endl;
-        }
-        else if (d[j] < b)
-        {
-          yy = true;
-        }
-      }
-      if (xx == false)
-      {
-        ans += count * d[i];
-        count = 0;
-      }
-      else if (xx == true && yy == true)
-      {
-        ans += count * d[i];
-        count = 0;
-      }
-      xx = false;
-      yy = false;
-    }
-    z = false;
-  }
+  // bool z = false, xx = false, yy = false;
+  // rep(i, 0, n)
+  // {
+  //   b = d[i];
+  //   rep(j, i + 1, n)
+  //   {
+  //     if (d[j] > b)
+  //     {
+  //       z = true;
+  //       // cout << "e: " << d[j] << endl;
+  //     }
+  //   }
+  //   // cout << "z: " << z << endl;
+  //   if (d[i] < a && z == true)
+  //   {
+  //     count += (ans / d[i]);
+  //     ans -= (ans / d[i]) * d[i];
+  //   }
+  //   else if (d[i] >= a)
+  //   {
+  //     // cout << "ans2: " << ans << " count: " << count << endl;
+  //     rep(j, i + 1, n)
+  //     {
+  //       if (d[j] > b)
+  //       {
+  //         xx = true;
+  //         // cout << "e: " << d[j] << endl;
+  //       }
+  //       else if (d[j] < b)
+  //       {
+  //         yy = true;
+  //       }
+  //     }
+  //     if (xx == false)
+  //     {
+  //       ans += count * d[i];
+  //       count = 0;
+  //     }
+  //     else if (xx == true && yy == true)
+  //     {
+  //       ans += count * d[i];
+  //       count = 0;
+  //     }
+  //     xx = false;
+  //     yy = false;
+  //   }
+  //   z = false;
+  // }
 
   out(ans);
 
